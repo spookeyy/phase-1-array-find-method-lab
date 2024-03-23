438c2585273aedc3a9f4e0f4fc69ee0d84741848
spookeyy <pangasmeshack@gmail.com>
2024-03-23 23:26:57 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+//Find method
+
+const testVar = {}
+
+//Solution code
+
+function testFunc() {
+    return "hi"
+  }
+
+  function superbowlWin(objArr) {
+    const winYear = objArr.find( e => e.result === "W")
+    if(winYear) {
+      return winYear.year
+    } else {
+      return undefined
+    }
+}
